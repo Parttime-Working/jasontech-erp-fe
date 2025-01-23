043b17e4fe25c8be558d19b0535362f027004c9b
jjasoncool <forwork.jan@gmail.com>
2025-01-23 16:24:01 +0800

    feat: add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <html>
+          <head>
+            <title>Dashboard</title>
+          </head>
+          <body>
+            <h1>Dashboard</h1>
+            <p>Welcome to the dashboard</p>
+          </body>
+        </html>
+      </div>
+    </div>
+  );
+}
